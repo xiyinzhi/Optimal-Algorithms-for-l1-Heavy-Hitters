4ad992f4241dbb318f8d502fd264c044eefcc8c3
xiyinzhi <452964062@qq.com>
2018-12-10 17:49:26 -0500

    initialize

--- project/src/MisraGries.java
+++ project/src/MisraGries.java
@@ -1,7 +1,7 @@
 /**
  * Created by xyz on 2018/12/10.
  */
-public class Near_Optimal {
+public class MisraGries {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- project/src/NearOptimal.java
+++ project/src/NearOptimal.java
@@ -1,7 +1,7 @@
 /**
  * Created by xyz on 2018/12/10.
  */
-public class Misra_Gries {
+public class NearOptimal {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
