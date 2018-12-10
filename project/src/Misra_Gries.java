7a0909fb0e6b26006c51c3c4ef01f12c19cd0bb1
xiyinzhi <452964062@qq.com>
2018-12-10 17:33:48 -0500

    initialize

--- project/src/Misra_Gries.java
+++ project/src/Misra_Gries.java
@@ -1,7 +1,7 @@
 /**
  * Created by xyz on 2018/12/10.
  */
-public class Main {
+public class Misra_Gries {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- project/src/Near_Optimal.java
+++ project/src/Near_Optimal.java
@@ -1,7 +1,7 @@
 /**
  * Created by xyz on 2018/12/10.
  */
-public class Main {
+public class Near_Optimal {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
