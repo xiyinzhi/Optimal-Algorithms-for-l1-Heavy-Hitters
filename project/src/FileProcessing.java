b06d8212c0f58ca34c6f015e92dfc906e7bf7d3c
xiyinzhi <452964062@qq.com>
2018-12-10 19:06:08 -0500

    file processing class

---
+++ project/src/FileProcessing.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by xyz on 2018/12/10.
+ */
+public class FileProcessing {
+
+    public List<String> readFile(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        List<String> list = new ArrayList<>();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempString;
+            while ((tempString = reader.readLine()) != null) {
+                list.add(tempString);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return list;
+    }
+
+    public void saveResult(Map<String, Integer> map, String fileName) throws IOException {
+        File file = new File(fileName + ".txt");
+        BufferedWriter out = new BufferedWriter(new FileWriter(file));
+//        out.write("食屎啦你");
+        for (String key : map.keySet()) {
+            out.write(key + "," + map.get(key) + "\r\n");
+        }
+        out.close();
+    }
+}
--- project/src/MisraGries.java
+++ project/src/MisraGries.java
@@ -1,9 +1,31 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by xyz on 2018/12/10.
  */
 public class MisraGries {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        int index = System.getProperty("user.dir").lastIndexOf("/");
+        String filePath = System.getProperty("user.dir").substring(0, index);
+
+        FileProcessing fileProcessing = new FileProcessing();
+        List<String> words = fileProcessing.readFile(filePath + "/test_article.txt");
+
+        MisraGries run = new MisraGries();
+        Map<String, Integer> map = run.misraGries(words);
+
+        fileProcessing.saveResult(map, "test_output");
+//        System.out.println("Hello World!");
     }
+
+    public Map<String, Integer> misraGries(List<String> words) {
+        Map<String, Integer> map = new HashMap<>();
+        return map;
+    }
+
 }
