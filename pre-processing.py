92fb39360981c7f7f3213808d8ded06beb27f660
zts212653 <773678591@qq.com>
2018-12-15 15:44:39 -0500

    Create pre-processing.py

---
+++ pre-processing.py
@@ -0,0 +1,69 @@
+"""
+Import
+"""
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+import enchant
+from collections import Counter
+from operator import itemgetter
+
+
+
+
+
+
+"""
+functions
+"""
+def cleanFile():
+    read = open('wikidump.txt', 'r')
+    write = open("small_wiki_streaming.txt",'w')
+    for s in read:
+        #s = read.readline()
+        words = nltk.word_tokenize(s)
+        d = enchant.Dict("en_US")
+        words=[word.lower() for word in words if (word.isalpha() and d.check(word))]
+
+
+
+        for word in words:
+            write.writelines(word)
+            write.write("\n")
+
+    read.close()
+    write.close()
+
+
+def wordCount():
+    read = open('test_input.txt', 'r')
+    write = open("test_input_sorted_word_count.txt",'w')
+
+    s = read.readlines()
+    #s.replace("\n","")
+    #words = nltk.word_tokenize(s)
+    word_count_dict = Counter(w for w in s)
+    sortedWords = sorted(word_count_dict.items(), key=itemgetter(1), reverse = True)
+
+
+    for w in sortedWords:
+        line = w[0].replace("\n","")
+        line+=" "+str(w[1])+"\n"
+        #print(line)
+        write.writelines(line)
+        #print(w[0],w[1])
+
+
+
+    read.close()
+    write.close()
+
+
+
+
+
+
+"""
+Main
+"""
+if __name__ == "__main__":
+    wordCount()
